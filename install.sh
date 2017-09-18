96682225c274b28b5ab3eb46c9c85745b3592b72
MottoWolf <j.motto.sanchez@gmail.com>
2017-09-18 10:20:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,25 @@
-aaaaa
+#!/bin/bash
+#Script que llama y ejecuta el resto de scripts
+
+echo "***********************************************"
+echo "*    Elige de entre las siguiente opciones    *"
+echo "*    1. Añadir usuario                        *"
+echo "*    2. Cifrar disco                          *"
+echo "*    6. Salir                                 *"
+echo "***********************************************"
+
+echo "Introduce un número"
+read option
+
+case $option in
+        1) sh 1.sh
+           rm ./1.sh
+        ;;
+        2) sh 2.sh
+           rm ./2.sh
+        ;;
+        6) exit
+        ;;
+esac
+
+sh ./metascript.sh
