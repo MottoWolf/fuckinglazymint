1997588ae93b1cc59301918b9d914cf3419821ee
Motto <jmmotto@paradigmadigital.com>
2017-10-31 08:46:27 +0100

    install modified

--- install.sh
+++ install.sh
@@ -1,25 +1,80 @@
 #!/bin/bash
-#Script que llama y ejecuta el resto de scripts
+# El demigrante
 
-echo "***********************************************"
-echo "*    Elige de entre las siguiente opciones    *"
-echo "*    1. Añadir usuario                        *"
-echo "*    2. Cifrar disco                          *"
-echo "*    6. Salir                                 *"
-echo "***********************************************"
+choice () {
+    local choice=$1
+    if [[ ${opts[choice]} ]] # toggle
+    then
+        opts[choice]=
+    else
+        opts[choice]="[*]"
+    fi
+}
 
-echo "Introduce un número"
-read option
+PS3='Please enter your choice: '
+while :
+do
+    clear
+    options=("Chrome ${opts[1]}" "Cinnamon ${opts[2]}" "Git ${opts[3]}" "Hangups ${opts[4]}" "KeepassX ${opts[5]}" "Spotify ${opts[6]}" "Steam ${opts[7]}" "Terminator ${opts[8]}" "Vim ${opts[9]}" "Virtualbox ${opts[10]}"  "INSTALL")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Chrome ${opts[1]}")
+                choice 1
+                break
+                ;;
+            "Cinnamon ${opts[2]}")
+                choice 2
+                break
+                ;;
+            "Git ${opts[3]}")
+                choice 3
+                break
+                ;;
+            "Hangups ${opts[4]}")
+                choice 4
+                break
+                ;;
+            "KeepassX ${opts[5]}")
+                choice 5
+                break
+                ;;
+            "Spotify ${opts[6]}")
+                choice 6
+                break
+                ;;
+            "Steam ${opts[7]}")
+                choice 7
+                break
+                ;;
+            "Terminator ${opts[8]}")
+                choice 8
+                break
+                ;;
+            "Vim ${opts[9]}")
+                choice 9
+                break
+                ;;
+            "Virtualbox ${opts[10]}")
+                choice 10
+                break
+                ;;
+            "INSTALL")
+                break 2
+                ;;
+            *) printf '%s\n' 'invalid option';;
+        esac
+    done
+done
 
-case $option in
-        1) sh 1.sh
-           rm ./1.sh
-        ;;
-        2) sh 2.sh
-           rm ./2.sh
-        ;;
-        6) exit
-        ;;
-esac
-
-sh ./metascript.sh
+printf '%s\n' 'Installing...'
+for opt in "${!opts[@]}"
+do
+    if [[ ${opts[opt]} ]]
+    then
+	 selected=$(printf '%s\n' "${options[opt - 1]}") 
+         #echo ansible-playbook playbook.yml --tags "$selected" | rev | cut -c 4- | rev
+	  echo ansible-playbook playbook.yml --tags $selected | rev | cut -c 4- | rev
+    fi
+done
+#ansible-playbook playbook.yml --tags 
