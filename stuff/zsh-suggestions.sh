58617190afe3005c40d5a4490f722ab75dcb0042
mottowolf <j.motto.sanchez@gmail.com>
2019-02-04 19:34:27 +0100

    new scripts

--- stuff/zsh-suggestions.sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-
--- stuff/zsh.sh
+++ stuff/zsh.sh
@@ -1,3 +1,10 @@
 sudo apt-get install zsh
+
 git clone https://github.com/powerline/fonts.git --depth=1 && cd fonts && ./install.sh && cd .. && rm -rf fonts
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+cp home/.bashrc /home/$USER/
+cp home/.zshrc /home/$USER/
